e20a279cf7681e5c4e92743a1dfd54d5051cb0dc
Orrett Bailey <orrettlb@gmail.com>
2017-09-16 04:27:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(arr, elem){
   return [elem, ...arr];
 }
+
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  return arr.unshift(elem);
+}
